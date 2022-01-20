1309ea33dd8331f0ecf3e0b79896d1fc1dd76a90
gustavo.maciel_accenture <gustavo.maciel_accenture@prestador.globo>
2022-01-20 15:44:04 -0300

    Test stage

---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestHelloWorldLen(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{{
+		name: "Hello World Len",
+		want: 11,
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			helloWorld := "Hello World"
+			got := len(helloWorld)
+
+			if got != tt.want {
+				t.Errorf("got %d, wanted %d", got, tt.want)
+			}
+		})
+	}
+}
